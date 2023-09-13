63aea041bf45808bc0743544f9ec589a2ce84625
Sahil Ahmed <zehraankhan7860@gmail.com>
2023-09-13 21:48:13 +0530

    backend commit

--- backend/verifyToken.js
+++ backend/verifyToken.js
@@ -2,16 +2,19 @@ const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
     const token = req.cookies.token
+    console.log("received token", token);
     if(!token){
+        console.log("token not found");
         return res.status(401).json("You are not authorized!")
     }
     jwt.verify(token, process.env.SECRET, async (err, data) => {
         if(err){
+            console.error('token verification error:', err);
             return res.status(403).json("Token is not valid!")
         }
         req.userId = data._id
-        // console.log("passed");
-        next()
+        console.log("Token verified, user ID:", data._id);
+        next();
     })
 }
 
