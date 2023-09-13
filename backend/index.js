2a8d1f516b7c94a71f42bbab094b27d9ec4232e2
Sahil Ahmed <zehraankhan7860@gmail.com>
2023-09-13 16:24:34 +0530

    backend push

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const connectDB=async()=>{
 app.use(express.json())
 dotenv.config()
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"https://blog-clone-zeddxx.vercel.app",credentials:true}))
+app.use(cors({origin:"https://blog-clone-git-main-zeddxx.vercel.app",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
