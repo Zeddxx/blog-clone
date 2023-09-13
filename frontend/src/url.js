a0cf41f23d62a92429fb224e46072b1c819f8e05
Sahil Ahmed <zehraankhan7860@gmail.com>
2023-09-13 16:22:59 +0530

    git init

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL="http://localhost:5000"
-export const IF ="http://localhost:5000/images/"
+export const URL="https://blog-clone.onrender.com"
+export const IF ="https://blog-clone.onrender.com/images/"
